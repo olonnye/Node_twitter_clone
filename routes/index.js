ec46523438c38ae2f0c4ec7ded5a8e7ede8e198d
boredasfawk <olonnyetaylor@gmail.com>
2019-08-28 21:06:46 -0700

    started user login
      added signup/login  pug  files and new routes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 let landing = require("../controllers/landing");
+let user = require("../controllers/user");
 
 /* GET home page. */
 router.get("/", landing.get_landing);
 router.post("/", landing.submit_lead);
+
+/* GET Leads page */
 
 router.get("/leads", landing.show_leads);
 
@@ -15,5 +18,9 @@ router.get("/lead/:lead_id/edit", landing.show_edit_lead);
 router.post("/lead/:lead_id/edit", landing.edit_lead);
 router.post("/lead/:lead_id/delete", landing.delete_lead);
 router.post("/lead/:lead_id/delete-json", landing.delete_lead_json);
+
+/*GET Login Page */
+router.get("login", user.show_login);
+router.get("/signup", user.show_signup);
 
 module.exports = router;
